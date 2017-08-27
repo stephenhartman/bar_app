dda33131c06f6dd02396465ba966f71e91e46152
stephenhartman <stephen.c.hartman@gmail.com>
2017-08-27 14:59:09 -0400

    Seed data

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $user = App\User::where('email', 'admin@example.com')->first();
+        if ($user === null) {
+            DB::table('users')->insert([
+                'name' => 'Admin',
+                'email' => 'admin@example.com',
+                'password' => bcrypt('secret'),
+                'is_admin' => 1,
+            ]);
+        }
+        $users = factory(App\User::class, 10)-create();
     }
 }
